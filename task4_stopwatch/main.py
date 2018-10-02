3f6ec9ae7d94af15297477d3378157630b6dcda9
phen0menon <garaganda1@mail.ru>
2018-10-02 21:53:38 +0300

    Add format() method to stopwatch

--- task4_stopwatch/main.py
+++ task4_stopwatch/main.py
@@ -1,31 +1,38 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 class Stopwatch(object):
+    STOPWATCH_TICKRATE = 100
     CANVAS_WIDTH = 500
     CANVAS_HEIGHT = 500
+
+    time = 0
+
     stopwatch_timer = None
     frame = None
 
-    time = 0
-    seconds = 0
-
     def __init__(self):
+        self.stopwatch_timer = simplegui.create_timer(100, self.stopwatch_change_state)
         self.frame = simplegui.create_frame("Stopwatch", self.CANVAS_WIDTH, self.CANVAS_HEIGHT)
         self.frame.set_draw_handler(self.draw)
         self.frame.add_button("Toggle", self.stopwatch_toggle)
         self.frame.add_button("Reset", self.stopwatch_reset)
-        self.stopwatch_timer = simplegui.create_timer(100, self.stopwatch_change_state)
 
     def draw(self, canvas):
-        canvas.draw_text(str(self.seconds), [self.CANVAS_WIDTH / 2, self.CANVAS_HEIGHT / 2], 18, 'white')
+        canvas.draw_text(self.format(), [self.CANVAS_WIDTH / 2, self.CANVAS_HEIGHT / 2], 18, 'white')
 
     def stopwatch_change_state(self):
-        self.time += 100
-        self.seconds = self.time // 1000
+        self.time += self.STOPWATCH_TICKRATE
 
     def format(self):
-        pass
-        # A:BC.D -> Minute:Seconds.Milliseconds
+        timestamp = self.time / 10
+
+        minutes = str(int(timestamp // (self.STOPWATCH_TICKRATE * 60)))
+        seconds = str(timestamp % (self.STOPWATCH_TICKRATE * 60) / self.STOPWATCH_TICKRATE)
+
+        if int(float(seconds)) < 10:
+            seconds = '0' + seconds[0:]
+
+        return minutes + ":" + seconds
 
     def stopwatch_toggle(self):
         if self.stopwatch_timer.is_running():
@@ -37,7 +44,6 @@ class Stopwatch(object):
         if self.stopwatch_timer.is_running():
             self.stopwatch_timer.stop()
         self.time = 0
-        self.seconds = 0
 
     def run(self):
         self.frame.start()
