55efd0faefa55d3ca6c5cd5dce0f4b6f4d988f1f
phen0menon <garaganda1@mail.ru>
2018-10-05 20:52:24 +0300

    Finish Pong

--- task5_pingpong/main.py
+++ task5_pingpong/main.py
@@ -1,5 +1,5 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-from random import randrange
+import random
 
 
 class Game(object):
@@ -31,22 +31,26 @@ class Game(object):
 
     def __init__(self):
         self.frame = simplegui.create_frame("Ping Pong", self.CANVAS_WIDTH, self.CANVAS_HEIGHT)
-
-        self.ball_pos = [self.CANVAS_WIDTH / 2, self.CANVAS_HEIGHT / 2]
-        self.ball_vel['x'] = randrange(120, 240)
-        self.ball_vel['y'] = randrange(60, 180)
-        self.paddles['left']['position'] = {
-            'start': {'x': self.PAD_WIDTH + 5, 'y': (self.CANVAS_HEIGHT / 2) - (self.PAD_HEIGHT / 2)},
-            'end': {'x': self.PAD_WIDTH + 5, 'y': (self.CANVAS_HEIGHT / 2) + (self.PAD_HEIGHT / 2)},
-        }
-        self.paddles['right']['position'] = {
-            'start': {'x': self.CANVAS_WIDTH - self.PAD_WIDTH - 6, 'y': (self.CANVAS_HEIGHT / 2) - (self.PAD_HEIGHT / 2)},
-            'end': {'x': self.CANVAS_WIDTH - self.PAD_WIDTH - 6, 'y': (self.CANVAS_HEIGHT) / 2 + (self.PAD_HEIGHT / 2)},
-        }
-
         self.frame.set_draw_handler(self.draw_game_window)
         self.frame.set_keyup_handler(self.keyup)
         self.frame.set_keydown_handler(self.keypress)
+        self.frame.add_button("Start Over", self.create_game, 200)
+        self.create_game()
+
+    def create_game(self):
+        self.reset_ball_state()
+        self.generate_ball_velocity()
+
+        self.paddles['left']['position'] = {
+            'start': {'x': self.PAD_WIDTH, 'y': (self.CANVAS_HEIGHT / 2) - (self.PAD_HEIGHT / 2)},
+            'end': {'x': self.PAD_WIDTH, 'y': (self.CANVAS_HEIGHT / 2) + (self.PAD_HEIGHT / 2)},
+        }
+        self.paddles['right']['position'] = {
+            'start': {'x': self.CANVAS_WIDTH - self.PAD_WIDTH, 'y': (self.CANVAS_HEIGHT / 2) - (self.PAD_HEIGHT / 2)},
+            'end': {'x': self.CANVAS_WIDTH - self.PAD_WIDTH, 'y': (self.CANVAS_HEIGHT) / 2 + (self.PAD_HEIGHT / 2)},
+        }
+        self.score_left = 0
+        self.score_right = 0
 
     def keypress(self, key):
         if key == simplegui.KEY_MAP["W"]:
@@ -77,12 +81,58 @@ class Game(object):
             [self.CANVAS_WIDTH - self.PAD_WIDTH, 0],
             [self.CANVAS_WIDTH - self.PAD_WIDTH, self.CANVAS_HEIGHT],
             1, "White")
+        canvas.draw_text(str(self.score_left) + " : " + str(self.score_right), [self.CANVAS_WIDTH / 2 - 20, 50], 24, 'white')
 
         self.generate_ball(canvas)
         self.generate_paddles(canvas)
 
+    def generate_ball_velocity(self):
+        allowed_values = [-4, -3, -2, 2, 3, 4]
+
+        self.ball_vel['x'] = random.randrange(2, 6)
+        self.ball_vel['y'] = random.choice(allowed_values)
+
     def generate_ball(self, canvas):
+        self.change_ball_state()
         canvas.draw_circle(self.ball_pos, self.BALL_RADIUS, 1, self.BALL_COLOR, self.BALL_COLOR)
+
+    def reset_ball_state(self):
+        self.ball_pos = [self.CANVAS_WIDTH / 2, self.CANVAS_HEIGHT / 2]
+
+    def increase_ball_velocity(self):
+        self.ball_vel['y'] *= 1.1
+        self.ball_vel['x'] *= 1.1
+
+    def change_ball_direction(self, direction):
+        self.generate_ball_velocity()
+
+        if direction == 'right':
+            self.score_right += 1
+        elif direction == 'left':
+            self.ball_vel['x'] = -self.ball_vel['x']
+            self.score_left += 1
+
+    def change_ball_state(self):
+        self.ball_pos[0] += self.ball_vel['x']
+        self.ball_pos[1] += self.ball_vel['y']
+
+        if (self.ball_pos[0] <= self.BALL_RADIUS):
+            if (self.ball_pos[1] <= self.paddles['left']['position']['end']['y'] and self.ball_pos[1] > self.paddles['left']['position']['start']['y']):
+                self.ball_vel['x'] *= -1
+                self.increase_ball_velocity()
+            else:
+                self.reset_ball_state()
+                self.change_ball_direction('right')
+
+        elif (self.ball_pos[0] >= self.CANVAS_WIDTH - self.BALL_RADIUS - 1):
+            if (self.ball_pos[1] <= self.paddles['right']['position']['end']['y'] and self.ball_pos[1] > self.paddles['right']['position']['start']['y']):
+                self.ball_vel['x'] *= -1
+            else:
+                self.reset_ball_state()
+                self.change_ball_direction('left')
+
+        if ((self.ball_pos[1] <= self.BALL_RADIUS) or (self.ball_pos[1] >= self.CANVAS_HEIGHT - self.BALL_RADIUS - 1)):
+            self.ball_vel['y'] *= -1
 
     def change_paddles_state(self):
         if ((self.paddles['left']['position']['start']['y'] + self.paddles['left']['velocity']) > 5
@@ -91,7 +141,7 @@ class Game(object):
             self.paddles['left']['position']['end']['y'] += self.paddles['left']['velocity']
 
         if ((self.paddles['right']['position']['start']['y'] + self.paddles['right']['velocity']) > 5
-                and (self.paddles['right']['position']['end']['y'] + self.paddles['left']['velocity']) < self.CANVAS_HEIGHT - 5):
+                and (self.paddles['right']['position']['end']['y'] + self.paddles['right']['velocity']) < self.CANVAS_HEIGHT - 5):
             self.paddles['right']['position']['start']['y'] += self.paddles['right']['velocity']
             self.paddles['right']['position']['end']['y'] += self.paddles['right']['velocity']
 
