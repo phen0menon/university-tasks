123e6f90dbcfe22b891a27a02e7c827cc4822636
phen0menon <garaganda1@mail.ru>
2019-02-23 13:31:45 +0300

    Add second task from lab

---
+++ prog/stepik_course/lab_2/task2.py
@@ -0,0 +1,27 @@
+def int2base(x, base):
+  digs = "0123456789abcdefghijklmnopqrstuvwxyz"
+  if x == 0: return digs[0]
+
+  digits = []
+
+  while x:
+    digits.append(digs[int(x % base)])
+    # x = int(x / base)
+    x = x // base
+
+  return ''.join(reversed(digits))
+
+def convert(num, count):
+  diff = count - len(num)
+
+  if diff > 0:
+    for _ in range(diff):
+      num.insert(0, "0")
+
+  return "".join(num)
+
+if __name__ == "__main__":
+  inputs = input().split(" ")
+
+  assoc_table = {"0": "A", "1": "C", "2": "G", "3": "T"}
+  print(*[assoc_table[j] for j in convert(list(str(int2base(int(inputs[0]), 4))), int(inputs[1]))], sep='')
