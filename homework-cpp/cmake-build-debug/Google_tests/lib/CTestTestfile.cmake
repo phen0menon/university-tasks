7a8567d8bd01ff973366f7534bca21f3c68fc278
phen0menon <garaganda21@gmail.com>
2019-10-02 16:10:49 +0300

    gitignore clean

--- homework-cpp/cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/university-tasks/homework-cpp/Google_tests/lib
-# Build directory: C:/Dev/university-tasks/homework-cpp/cmake-build-debug/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- homework-cpp/cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/university-tasks/homework-cpp/Google_tests/lib/googletest
-# Build directory: C:/Dev/university-tasks/homework-cpp/cmake-build-debug/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
